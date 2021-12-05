2828273617a1616bc109ea012672857f6f7ef06f
Albert <albert.rachwal@pollub.edu.pl>
2021-12-05 11:05:37 +0100

    init

---
+++ Fibonacci_in_Cloud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fibonacci_in_Cloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
